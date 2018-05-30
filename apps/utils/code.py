5b0bd92d859fbd5cfef80a80066e45a1fcbc0a77
倪兴国 <1025464043@qq.com>
2018-05-30 17:26:03 +0800

    Add files via upload

--- apps/utils/code.py
+++ apps/utils/code.py
@@ -1,24 +1,20 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-__author__ = "xin nix"
 import re
-from random import Random
-from urllib import request
-import requests
-from PIL import Image, ImageDraw, ImageFont, ImageFilter
-import json
-from django.shortcuts import render, HttpResponse
 import os
-from django.core.mail import send_mail
-
-from chartsite import settings
-from chartsite.settings import BASE_DIR, EMAIL_FROM, HOST
-from apps.users.models import EmailCode
+import json
+import requests
 import smtplib
 import random
 from email.mime.text import MIMEText
 from email.utils import formataddr
+from urllib.request import quote, unquote
 
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+from django.shortcuts import  HttpResponse
+from django.core.mail import send_mail
+
+from chartsite import settings
+from chartsite.settings import BASE_DIR, EMAIL_FROM, HOST
+from users.models import EmailCode
 
 
 # Create your views here.
@@ -27,116 +23,6 @@ _upper_cases = _letter_cases.upper()  # 大写字母
 _numbers = ''.join(map(str, range(3, 10)))  # 数字
 init_chars = ''.join((_letter_cases, _upper_cases, _numbers))
 TFF_PATH = os.path.join(BASE_DIR, 'static', 'fonts', 'Monaco.TTF')
-
-
-class ImgEmailCode(object):
-    """
-    @todo: 生成验证码图片
-    @param size: 图片的大小，格式（宽，高），默认为(120, 30)
-    @param chars: 允许的字符集合，格式字符串
-    @param img_type: 图片保存的格式，默认为GIF，可选的为GIF，JPEG，TIFF，PNG
-    @param mode: 图片模式，默认为RGB
-    @param bg_color: 背景颜色，默认为白色
-    @param fg_color: 前景色，验证码字符颜色，默认为蓝色#0000FF
-    @param font_size: 验证码字体大小
-    @param font_type: 验证码字体，默认为 ae_AlArabiya.ttf
-    @param length: 验证码字符个数
-    @param draw_lines: 是否划干扰线
-    @param n_lines: 干扰线的条数范围，格式元组，默认为(data, 2)，只有draw_lines为True时有效
-    @param draw_points: 是否画干扰点
-    @param point_chance: 干扰点出现的概率，大小范围[0, 100]
-    @return: [0]: PIL Image实例
-    @return: [data]: 验证码图片中的字符串
-    """
-    def __init__(self,
-                 size=(120, 38),
-                 chars=init_chars,
-                 img_type="GIF",
-                 mode="RGB",
-                 bg_color=(255, 255, 255),
-                 fg_color=(0, 0, 255),
-                 font_size=18,
-                 font_type=TFF_PATH,
-                 length=4,
-                 draw_lines=True,
-                 n_line=(1, 2),
-                 draw_points=True,
-                 point_chance=2,
-                 code='1234'):
-        self.size = size,
-        self.chars = chars,
-        self.img_type = img_type,
-        self.mode = mode,
-        self.bg_color = bg_color,
-        self.fg_color = fg_color,
-        self.font_size = font_size,
-        self.font_type = font_type,
-        self.length = length,
-        self.draw_lines = draw_lines,
-        self.n_line = n_line,
-        self.draw_points = draw_points,
-        self.point_chance = point_chance
-        self.width, self.height = size  # 宽， 高
-        self.img = Image.new(mode, size, bg_color)  # 创建图形
-        self.draw = ImageDraw.Draw(self.img)  # 创建画笔
-        self.code = code
-
-    def code_to_img(self, code):
-        font = ImageFont.truetype(self.font_type[0], self.font_size[0])
-        font_width, font_height = font.getsize(code)
-        xy = ((self.width - font_width) / 3, (self.height - font_height) / 3)
-        self.draw.text(xy, code, font=font, fill=self.fg_color[0])
-
-    def create_lines(self):
-        """绘制干扰线"""
-        line_long = random.randint(*self.n_line[0])  # 干扰线条数
-
-        for i in range(line_long):
-            # 起始点
-            begin = (random.randint(0, self.width), random.randint(0, self.height))
-            # 结束点
-            end = (random.randint(0, self.width), random.randint(0, self.height))
-            self.draw.line([begin, end], fill=(0, 0, 0))
-
-    def create_points(self):
-        """绘制干扰点"""
-        chance = min(100, max(0, int(self.point_chance)))  # 大小限制在[0, 100]
-
-        for w in range(self.width):
-            for h in range(self.height):
-                tmp = random.randint(0, 100)
-                if tmp > 100 - chance:
-                    self.draw.point((w, h), fill=(0, 0, 0))
-
-    def lines_points(self):
-        if self.draw_lines:
-            self.create_lines()
-        if self.draw_points:
-            self.create_points()
-
-    def distort(self):
-        # 图形扭曲参数
-        params = [1 - float(random.randint(1, 2)) / 100,
-                  0,
-                  0,
-                  0,
-                  1 - float(random.randint(1, 10)) / 100,
-                  float(random.randint(1, 2)) / 500,
-                  0.001,
-                  float(random.randint(1, 2)) / 500
-                  ]
-        self.img = self.img.transform(self.size[0], Image.PERSPECTIVE, params)  # 创建扭曲
-        self.img = self.img.filter(ImageFilter.EDGE_ENHANCE_MORE)  # 滤镜，边界加强（阈值更大）
-        return self.img
-
-    def made_code_img(self):
-        self.code_to_img(self.code)
-        self.lines_points()
-        self.distort()
-        image = self.code + '.png'
-        img_path = os.path.join(BASE_DIR, 'media', 'chartsite', 'captcha', image)
-        self.img.save(img_path)
-        return img_path, image
 
 
 def str_tool(length):
@@ -254,7 +140,7 @@ def check_img_code(request):
 
 def sendactemail(host, username, emailaddr):
     data = {'status': False, 'msg': ''}
-    # 第三方 SMTP 服务
+    # 第三方SMTP服务
     my_sender = '1025464043@qq.com'  # 发件人邮箱账号
     my_pass = 'ehtcmrqluckmbgab'  # 发件人邮箱密码
     receivers = [emailaddr, ]  # 收件人邮箱账号，我这边发送给自己
@@ -278,10 +164,10 @@ def sendactemail(host, username, emailaddr):
     农业统计数据</td></tr><tr><td colspan="2" style="padding:8px0 28px;color:#999999; font-size:12px;text-align: right;
     padding-right: 40px;" width="360" height="14">此为系统邮件请勿回复</td></tr></tbody></table></td></tr><td><table 
     width="500" height="40" cellspacing="0" cellpadding="0" border="0" align="center"></table></td></tbody></table>"""
-    username = request.quote(username)
+    username = quote(username)
     url = "active-email?username={}&email={}".format(username, emailaddr)
     mail_msg = re.sub("active-email", url, mail_msg)
-    username = request.unquote(username)
+    username = unquote(username)
     mail_msg = re.sub("尊敬的", "尊敬的" + username, mail_msg)
     mail_msg = re.sub("127.0.0.data:8000", host, mail_msg)
     try:
@@ -298,23 +184,6 @@ def sendactemail(host, username, emailaddr):
     except Exception as e:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
         data['msg'] = e
     return data
-
-
-class SmsPhoneCode(object):
-
-    def __init__(self, api_key):
-        self.api_key = api_key
-        self.single_send_url = "https://sms.yunpian.com/v2/sms/single_send.json"
-
-    def send_msg(self, code, mobile):
-        parmas = {
-            'apikey': self.api_key,
-            "mobile": mobile,
-            "text": "【农业统计数据】您的验证码是{}".format(code)
-        }
-        response = requests.post(self.single_send_url, data=parmas)
-        re_dict = json.loads(response.text)
-        return re_dict
 
 
 def send_email(email, code, send_type="register"):
@@ -343,7 +212,7 @@ def send_email(email, code, send_type="register"):
 
     if send_type == "activate":
         email_title = title + "账号激活链接"
-        url = '{host}//user/active/{code}'.format(host=HOST, code=code)
+        url = '{host}/user/active/{code}'.format(host=HOST, code=code)
         email_body = email_body.format(host=HOST, notice="请点击下面的链接激活你的账号(此链接有效期为24小时):", url=url, email=email)
         send_status = send_mail(email_title, email_body, EMAIL_FROM, [email], html_message=email_body)
         if send_status:
@@ -367,3 +236,129 @@ def send_email(email, code, send_type="register"):
             data = True
         return data
 
+
+class SmsPhoneCode(object):
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.single_send_url = "https://sms.yunpian.com/v2/sms/single_send.json"
+
+    def send_msg(self, code, mobile):
+        parmas = {
+            'apikey': self.api_key,
+            "mobile": mobile,
+            "text": "【农业统计数据】您的验证码是{}".format(code)
+        }
+        response = requests.post(self.single_send_url, data=parmas)
+        re_dict = json.loads(response.text)
+        return re_dict
+
+
+class ImgEmailCode(object):
+    """
+    @todo: 生成验证码图片
+    @param size: 图片的大小，格式（宽，高），默认为(120, 30)
+    @param chars: 允许的字符集合，格式字符串
+    @param img_type: 图片保存的格式，默认为GIF，可选的为GIF，JPEG，TIFF，PNG
+    @param mode: 图片模式，默认为RGB
+    @param bg_color: 背景颜色，默认为白色
+    @param fg_color: 前景色，验证码字符颜色，默认为蓝色#0000FF
+    @param font_size: 验证码字体大小
+    @param font_type: 验证码字体，默认为 ae_AlArabiya.ttf
+    @param length: 验证码字符个数
+    @param draw_lines: 是否划干扰线
+    @param n_lines: 干扰线的条数范围，格式元组，默认为(data, 2)，只有draw_lines为True时有效
+    @param draw_points: 是否画干扰点
+    @param point_chance: 干扰点出现的概率，大小范围[0, 100]
+    @return: [0]: PIL Image实例
+    @return: [data]: 验证码图片中的字符串
+    """
+    def __init__(self,
+                 size=(120, 38),
+                 chars=init_chars,
+                 img_type="GIF",
+                 mode="RGB",
+                 bg_color=(255, 255, 255),
+                 fg_color=(0, 0, 255),
+                 font_size=18,
+                 font_type=TFF_PATH,
+                 length=4,
+                 draw_lines=True,
+                 n_line=(1, 2),
+                 draw_points=True,
+                 point_chance=2,
+                 code='1234'):
+        self.size = size,
+        self.chars = chars,
+        self.img_type = img_type,
+        self.mode = mode,
+        self.bg_color = bg_color,
+        self.fg_color = fg_color,
+        self.font_size = font_size,
+        self.font_type = font_type,
+        self.length = length,
+        self.draw_lines = draw_lines,
+        self.n_line = n_line,
+        self.draw_points = draw_points,
+        self.point_chance = point_chance
+        self.width, self.height = size  # 宽， 高
+        self.img = Image.new(mode, size, bg_color)  # 创建图形
+        self.draw = ImageDraw.Draw(self.img)  # 创建画笔
+        self.code = code
+
+    def code_to_img(self, code):
+        font = ImageFont.truetype(self.font_type[0], self.font_size[0])
+        font_width, font_height = font.getsize(code)
+        xy = ((self.width - font_width) / 3, (self.height - font_height) / 3)
+        self.draw.text(xy, code, font=font, fill=self.fg_color[0])
+
+    def create_lines(self):
+        """绘制干扰线"""
+        line_long = random.randint(*self.n_line[0])  # 干扰线条数
+
+        for i in range(line_long):
+            # 起始点
+            begin = (random.randint(0, self.width), random.randint(0, self.height))
+            # 结束点
+            end = (random.randint(0, self.width), random.randint(0, self.height))
+            self.draw.line([begin, end], fill=(0, 0, 0))
+
+    def create_points(self):
+        """绘制干扰点"""
+        chance = min(100, max(0, int(self.point_chance)))  # 大小限制在[0, 100]
+
+        for w in range(self.width):
+            for h in range(self.height):
+                tmp = random.randint(0, 100)
+                if tmp > 100 - chance:
+                    self.draw.point((w, h), fill=(0, 0, 0))
+
+    def lines_points(self):
+        if self.draw_lines:
+            self.create_lines()
+        if self.draw_points:
+            self.create_points()
+
+    def distort(self):
+        # 图形扭曲参数
+        params = [1 - float(random.randint(1, 2)) / 100,
+                  0,
+                  0,
+                  0,
+                  1 - float(random.randint(1, 10)) / 100,
+                  float(random.randint(1, 2)) / 500,
+                  0.001,
+                  float(random.randint(1, 2)) / 500
+                  ]
+        self.img = self.img.transform(self.size[0], Image.PERSPECTIVE, params)  # 创建扭曲
+        self.img = self.img.filter(ImageFilter.EDGE_ENHANCE_MORE)  # 滤镜，边界加强（阈值更大）
+        return self.img
+
+    def made_code_img(self):
+        self.code_to_img(self.code)
+        self.lines_points()
+        self.distort()
+        image = self.code + '.png'
+        img_path = os.path.join(BASE_DIR, 'media', 'chartsite', 'captcha', image)
+        self.img.save(img_path)
+        return img_path, image
